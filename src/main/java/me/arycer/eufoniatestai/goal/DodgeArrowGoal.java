220b8dc98fed1d1b0aa8f5171109864bcb0b9c1a
Michel Mendoza <contacto@arycer.me>
2023-05-25 01:33:37 +0200

    Habilidad de esquivar flechas mejorada
    Took 1 minute

--- src/main/java/me/arycer/eufoniatestai/goal/DodgeArrowGoal.java
+++ src/main/java/me/arycer/eufoniatestai/goal/DodgeArrowGoal.java
@@ -1,12 +1,12 @@
 package me.arycer.eufoniatestai.goal;
 
+import me.arycer.eufoniatestai.Main;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 
 import java.util.EnumSet;
 
@@ -31,21 +31,17 @@ public class DodgeArrowGoal extends Goal {
     }
 
     @Override
-    public void start() {
-    }
-
-    @Override
-    public void stop() {
-        super.stop();
-    }
-
-    @Override
     public void tick() {
         if (this.entity.getNavigation().isFollowingPath()) return;
-        BlockPos dodgeLocation = getDodgeLocation();
-        if (dodgeLocation == null) return;
 
-        this.entity.getNavigation().startMovingTo(dodgeLocation.getX(), dodgeLocation.getY(), dodgeLocation.getZ(), this.speed * 1.5f);
+        int lr = (int) (Math.random() * 2);
+        int distance_i = (int) (Math.random() * 2) + 2;
+        int distance_k = (int) (Math.random() * 2) + 2;
+
+        BlockPos strafeLocation = this.entity.getBlockPos().add(lr == 0 ? distance_i : -distance_i, 0, lr == 1 ? distance_k : -distance_k);
+        this.entity.getNavigation().startMovingTo(strafeLocation.getX(), strafeLocation.getY(), strafeLocation.getZ(), this.speed * 1.25f);
+
+        Main.LOGGER.info(String.format("DodgeArrowGoal: %s trying to dodge an arrow!", this.entity.getName().getString()));
     }
 
     private boolean targetShootingBow() {
@@ -57,35 +53,5 @@ public class DodgeArrowGoal extends Goal {
 
         if (!mainHandStack.getItem().equals(Items.BOW) && !offHandStack.getItem().equals(Items.BOW)) return false;
         return target.isUsingItem();
-    }
-
-    private BlockPos getDodgeLocation() {
-        LivingEntity target = this.entity.getTarget();
-        if (target == null) return null;
-
-        BlockPos entityPos = this.entity.getBlockPos();
-
-        int distanceToTarget = (int) this.entity.distanceTo(target);
-        if (distanceToTarget < 10) {
-            int runDistance = 10 - distanceToTarget;
-            Vec3d targetPos = target.getPos();
-
-            double x = entityPos.getX() - targetPos.getX();
-            double z = entityPos.getZ() - targetPos.getZ();
-            double angle = Math.atan2(z, x);
-
-            double newX = entityPos.getX() + runDistance * Math.cos(angle);
-            double newZ = entityPos.getZ() + runDistance * Math.sin(angle);
-
-            return new BlockPos((int) newX, (int) target.getY(), (int) newZ);
-        }
-
-        int distance = target.getRandom().nextInt(4) + 4;
-        double angle = target.getRandom().nextDouble() * 2 * Math.PI;
-
-        double x = entityPos.getX() + distance * Math.cos(angle);
-        double z = entityPos.getZ() + distance * Math.sin(angle);
-
-        return new BlockPos((int) x, (int) target.getY(), (int) z);
     }
 }
