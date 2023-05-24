1e99c3358af79fb5f4bdf034f9ffba8133d88ff5
Michel Mendoza <contacto@arycer.me>
2023-05-25 01:34:05 +0200

    Habilidad de construir añadida
    Took 28 seconds

---
+++ src/main/java/me/arycer/eufoniatestai/goal/TowerUpGoal.java
@@ -0,0 +1,90 @@
+package me.arycer.eufoniatestai.goal;
+
+import me.arycer.eufoniatestai.Main;
+import net.minecraft.block.Block;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class TowerUpGoal extends Goal {
+    protected final MobEntity entity;
+
+    public TowerUpGoal(MobEntity entity) {
+        this.entity = entity;
+    }
+
+    @Override
+    public boolean canStart() {
+        if (this.entity.getNavigation().isFollowingPath()) return false;
+
+        BreakBlockGoal breakBlockGoal = new BreakBlockGoal(this.entity);
+        return !breakBlockGoal.canStartMining() && getBlocksItemStack() != null;
+    }
+
+    @Override
+    public boolean shouldContinue() {
+        return canStart();
+    }
+
+    @Override
+    public void start() {
+        towerUp();
+
+        Main.LOGGER.info(String.format("TowerUpGoal from %s started!", this.entity.getName().getString()));
+    }
+
+    @Override
+    public void stop() {
+        Main.LOGGER.info(String.format("TowerUpGoal from %s stopped!", this.entity.getName().getString()));
+    }
+
+    private ItemStack getBlocksItemStack() {
+        ItemStack mainHandStack = this.entity.getMainHandStack();
+        ItemStack offHandStack = this.entity.getOffHandStack();
+
+        if (mainHandStack.isEmpty() && offHandStack.isEmpty()) return null;
+
+        if (mainHandStack.getItem() instanceof BlockItem) return mainHandStack;
+        if (offHandStack.getItem() instanceof BlockItem) return offHandStack;
+
+        return null;
+    }
+
+    private Block getHandBlock() {
+        ItemStack blocksItemStack = getBlocksItemStack();
+        if (blocksItemStack == null) return null;
+
+        Item item = blocksItemStack.getItem();
+        if (!(item instanceof BlockItem blockItem)) return null;
+
+        return blockItem.getBlock();
+    }
+
+    private void towerUp() {
+        Block handBlock = getHandBlock();
+        if (handBlock == null) return;
+
+        LivingEntity target = this.entity.getTarget();
+        if (target == null) return;
+
+        BlockPos targetPos = target.getBlockPos();
+        BlockPos entityPos = this.entity.getBlockPos();
+
+        if (targetPos.getY() < entityPos.getY()) return;
+
+        World world = this.entity.getWorld();
+
+        this.entity.getJumpControl().setActive();
+        world.setBlockState(entityPos, handBlock.getDefaultState());
+
+        ItemStack blocksItemStack = getBlocksItemStack();
+        if (blocksItemStack == null) return;
+
+        blocksItemStack.decrement(1);
+    }
+}
